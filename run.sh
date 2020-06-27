16d1d5cba3e96ae0ec035a4aa2b637bbdbbe1fbf
Desarrollo Cast <desarrollo@cast.com.py>
2020-06-27 11:29:32 -0400

    Ajustes en el Docker File, pom y run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 docker container rm cobranzas
 
-docker run --publish 8082:8082 -d  --name cobranzas cobranzas-sb
+docker run --publish 8082:8080 --network=dnet1 -d --rm --name cobranzas cobranzas-sb
+
+//--network=dnet1 para que le vea a la red que se crea con Docker.
